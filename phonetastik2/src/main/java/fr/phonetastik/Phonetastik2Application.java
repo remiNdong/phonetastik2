671239898ce3cab62f76f0eb9d3a1f034f3fbbe7
remiNdong <ndongremi@aol.fr>
2023-03-25 12:45:55 +0100

    first commit

---
+++ phonetastik2/src/main/java/fr/phonetastik/Phonetastik2Application.java
@@ -0,0 +1,39 @@
+package fr.phonetastik;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+import fr.phonetastik.utils.StorageService;
+
+@SpringBootApplication
+@ComponentScan("fr.phonetastik")
+public class Phonetastik2Application extends SpringBootServletInitializer  {
+	
+	//implements CommandLineRunner
+	
+	@Autowired
+	StorageService storageService;
+	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Phonetastik2Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phonetastik2Application.class, args);
+	}
+
+	/*
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
+	*/
+
+}
---
+++ phonetastik2/src/test/java/fr/phonetastik/Phonetastik2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.phonetastik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phonetastik2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
